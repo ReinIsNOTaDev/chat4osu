80498e8cba9405daa34156e0b3445c0f3b4d23de
Uwe Wiemer <hallowatcher@gmail.com>
2018-09-13 17:38:36 +0200

    add redirect to chats when logged in

--- src/app/store/states/auth.state.ts
+++ src/app/store/states/auth.state.ts
@@ -2,6 +2,7 @@ import { State, Action, StateContext } from '@ngxs/store';
 import { Login, LoginSuccess, LoginFailed } from '../actions/auth.actions';
 import produce from 'immer';
 import { IrcService } from '../../providers/irc.service';
+import { Router } from '@angular/router';
 
 export interface AuthStateModel {
   username: string;
@@ -18,7 +19,7 @@ export interface AuthStateModel {
   }
 })
 export class AuthState {
-  constructor(public irc: IrcService) {}
+  constructor(public irc: IrcService, public router: Router) {}
 
   @Action(Login)
   async login(ctx: StateContext<AuthStateModel>, action: Login) {
@@ -36,6 +37,9 @@ export class AuthState {
 
   @Action(LoginSuccess)
   loginSuccess(ctx: StateContext<AuthStateModel>, action: LoginSuccess) {
+    // TODO: Replace with store implementation
+    this.router.navigate(['/chat']);
+
     ctx.setState(
       produce(ctx.getState(), draft => {
         draft.loggedIn = true;
