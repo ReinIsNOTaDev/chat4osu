239225805c55f0766509211273b92d0705fb1495
Uwe Wiemer <hallowatcher@gmail.com>
2019-03-05 15:29:49 +0100

    fix unit tests

--- src/app/components/input-bar/input-bar.component.spec.ts
+++ src/app/components/input-bar/input-bar.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputBarComponent } from './input-bar.component';
 import { NgxsModule } from '@ngxs/store';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
 
 describe('InputBarComponent', () => {
   let component: InputBarComponent;
@@ -10,7 +12,7 @@ describe('InputBarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [InputBarComponent],
-      imports: [NgxsModule.forRoot()],
+      imports: [NgxsModule.forRoot(), NgxsFormPluginModule.forRoot(), ReactiveFormsModule],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
